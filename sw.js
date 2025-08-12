202e2adfe4a3ec3e38aa16e88e82f6c3b311d53e
DrGoo1 <umbogumbo1@gmail.com>
2025-08-12 15:44:24 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,43 @@
-const CACHE='vascular-quiz-jg-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{const url=new URL(e.request.url);
-  if(e.request.method==='GET'&&url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{return caches.open(CACHE).then(c=>{c.put(e.request,res.clone());return res;});}).catch(()=>caches.match('./index.html'))));
-  }});
+const CACHE = 'vascular-quiz-jg-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './questions.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await c.addAll(ASSETS);
+  })());
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method === 'GET' && url.origin === location.origin) {
+    e.respondWith((async () => {
+      const cached = await caches.match(e.request);
+      if (cached) return cached;
+      try {
+        const res = await fetch(e.request);
+        const c = await caches.open(CACHE);
+        c.put(e.request, res.clone());
+        return res;
+      } catch {
+        return caches.match('./index.html');
+      }
+    })());
+  }
+});
